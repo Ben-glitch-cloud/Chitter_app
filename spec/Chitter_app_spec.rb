ce80dfe206943d966b123fbb30791d78e7bc22d2
Benedict Lawrence <BTLawrence44@gmail.com>
2021-04-02 12:49:28 +0100

    more testing

--- spec/Chitter_app_spec.rb
+++ spec/Chitter_app_spec.rb
@@ -1,11 +1,23 @@
 require './lib/Chitter_app.rb' 
 
 describe Chitters_app do
-    describe 'add' do  
-        it 'a new chitter to the Chitter app' do
+    describe 'store' do  
+        it 'Chitters in the main app' do
             chitter = Chitters_app.new 
-            p chitter.add('Hello World', '01/01/2021')
-            
+            chitter.add('Hello World', '01/01/2021')
+            chitter.store.each do |chitter|  
+                expect(chitter.mes).to include('Hello World') 
+                expect(chitter.sent_time).to include('01/01/2021')
+            end 
         end 
-    end 
+    end  
+    describe 'add' do
+        it 'a new Chitter to the app' do
+            chitter = Chitters_app.new 
+            chitter.add('Hello Chitter', '02/03/2021') 
+            chitter.add('Whats going here', '02/03/2021') 
+            chitter.add('Building tests', '02/03/2021') 
+            expect(chitter.store.map { |chitter| chitter.mes} ).to eq(["Hello Chitter", "Whats going here", "Building tests"])
+        end
+    end  
 end
--- spec/feature/veiwing_chitter_spec.rb
+++ spec/feature/veiwing_chitter_spec.rb
@@ -1,10 +1,23 @@
-require 'pg'
 
-feature 'Viewing Chitter' do
-    scenario 'as a user' do
-        # connection = PG.connect(dbname: 'chitter_manager_test') 
-        # connection.exec("INSERT INTO message (mes, sent_time) VALUES('Hello world', '01/01/2021')") 
-        # chitter = Chitter.new 
-        # expect(chitter.store).to include('Hello World')
+feature 'New Chitter' do
+    scenario 'created by the user' do
+        visit('/chitter') 
+        click_link 'New Chitter' 
+        fill_in 'chit', with: 'Hello World' 
+        click_button 'Submit'  
+        expect(page).to have_content "New Chitter\nLog out\nHello World\n#{Time.new.strftime("%d/%m/%Y")}"
+    end 
+end  
+
+
+
+feature 'Delete Chitter' do
+    scenario 'from chitter' do
+        visit('/chitter') 
+        click_link 'New Chitter' 
+        fill_in 'chit', with: 'Whats up' 
+        click_button 'Submit' 
+        click_button 'Delete' 
+        expect(page).to have_no_content "Whats up"
     end
 end
